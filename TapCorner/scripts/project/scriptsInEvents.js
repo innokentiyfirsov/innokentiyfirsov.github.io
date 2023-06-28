f5b440f2798889c2bada3777e57832df80bba0fc
innokentiyfirsov <136494724+innokentiyfirsov@users.noreply.github.com>
2023-06-28 17:57:07 +0300

    Add files via upload

---
+++ TapCorner/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event3_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#383054'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
