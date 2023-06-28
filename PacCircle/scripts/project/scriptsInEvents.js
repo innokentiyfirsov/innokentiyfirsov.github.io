c0b7feb34ed8f4adeef66cb02c4e8dec2b734b24
innokentiyfirsov <136494724+innokentiyfirsov@users.noreply.github.com>
2023-06-28 17:58:09 +0300

    Add files via upload

---
+++ PacCircle/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event4_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#655889'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
